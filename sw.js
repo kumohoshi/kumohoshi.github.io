8289d320ebe82dac2a0a6502ac74ce6e749eaead
YXXY <1243586662@qq.com>
2025-09-21 23:13:13 +0800

    Site updated: 2025-09-21 23:13:12

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1758466785151";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1758467549968";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
